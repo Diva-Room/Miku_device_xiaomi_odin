dce747ae8640ee6ec09e1c5b2a9f34284c2898f1
AmeChanRain <downloadbot007@gmail.com>
2025-07-19 18:50:57 +0800

    odin: Enable adb by default

--- device.mk
+++ device.mk
@@ -109,6 +109,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml \
 
+# Adb
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default
